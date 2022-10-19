7cfbf2abf09f7944e7eb152f1abac11de36de6f6
HassanAfroGod <90245025+HassanAfroGod@users.noreply.github.com>
2022-10-19 15:41:34 +0200

    Kommit  frammåt

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,6 +21,19 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
+        Vector3 playerposition = playerKart.transform.position;
+        Vector3 checkpointPosition = levelCheckpoints[0].position;
+
+
+        Vector3 diff = new Vector3(playerposition.x - checkpointPosition.x + playerposition.y - checkpointPosition.y, checkpointPosition.z - checkpointPosition.z);
+
+
+
+        float distanceBetween2Objects = Mathf.Sqrt(Mathf.Pow(diff.x, 2) + Mathf.Pow(diff.y, 2)) + Mathf.Pow(diff.z, 2);
+
+        Debug.Log(distanceBetween2Objects);
+
         
+
     }
 }
